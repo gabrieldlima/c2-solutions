872267786d84685446dd22dc05eca0d015f25b69
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-06-14 19:03:44 -0300

    feat: add exercise_03 from chapter 02

---
+++ src/chapter_02/exercises/exercise_03.c
@@ -0,0 +1,5 @@
+/*
+Condense the dweight.c program by (1) replacing the assignments to height,
+length, and width with initializers and (2) removing the weight variable, ins-
+tead of calculating (volume+165) / 166 within the last printf.
+*/
