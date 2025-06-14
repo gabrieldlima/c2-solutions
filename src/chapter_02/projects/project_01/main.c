5064577b9ee793bac1fda6b80ec066c64a46c2d5
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-06-14 19:08:54 -0300

    fix: braces position

--- src/chapter_02/projects/project_01/main.c
+++ src/chapter_02/projects/project_01/main.c
@@ -12,7 +12,8 @@ Write a program that uses printf to display the following picture on the screen:
 
 #include <stdio.h>
 
-int main(void) {
+int main(void)
+{
     printf("       *\n");
     printf("      *\n");
     printf("     *\n");
