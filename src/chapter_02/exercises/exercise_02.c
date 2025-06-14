ce81896ee954e2cd26a0053f1394dc2db80c0a7c
Gabriel Lima <gabrielgsdlima@gmail.com>
2025-06-14 19:03:36 -0300

    feat: add exercise_02 from chapter 02

---
+++ src/chapter_02/exercises/exercise_02.c
@@ -0,0 +1,32 @@
+/*
+Consider the following program:
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Parkinson's Law: \nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+    return 0;
+}
+
+/*
+(a) Identify the directives and statements in this program.
+
+Directives:
+    #include <stdio.h>
+
+Statements:
+    printf("Parkinson's Law: \nWork expands so as to ");
+    printf("fill the time\n");
+    printf("available for its completion.\n");
+    return 0;
+
+(b) What output does the program produce?
+
+Parkinson's Law:
+Work expands so as to fill the time
+available for its completion.
+*/
